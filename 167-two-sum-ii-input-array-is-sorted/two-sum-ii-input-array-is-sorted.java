546792e87d558e47b567d55970592756de3f425a
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-01-18 11:31:20 +0530

    Time: 2 ms (96.33%) | Memory: 48.6 MB (30.69%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -3,7 +3,7 @@ class Solution {
         int i = 0;
         int j = numbers.length - 1;
 
-        while (i < j) {
+           while (i < j) {
             int sum = numbers[i] + numbers[j];
             if(sum < target){
                i++;
@@ -13,9 +13,11 @@ class Solution {
             }
             else{
                 return new int[] {i + 1 , j + 1};
+                }
             }
-        }
 
         return new int[] {}; 
     }
 }
+
+       
