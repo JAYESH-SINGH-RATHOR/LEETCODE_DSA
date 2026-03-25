702aad332d1294e32811c1c65b994e4783ef7793
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-03-25 22:58:33 +0530

    Time: 0 ms (100.00%) | Memory: 42.7 MB (83.26%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,21 +1,44 @@
 class Solution {
     public int rob(int[] nums) {
          int n = nums.length;
-         return robtab(nums);
-    }
-    int robtab(int nums[]){
-        int n = nums.length;
-        if(n == 1){
-            return nums[0];
-        }
         int dp[] = new int[n];
         Arrays.fill(dp , -1);
-        dp[0] = nums[0];
-        dp[1] = Math.max(nums[0] ,nums[1]);
-        for(int i = 2; i < n; i++){
-            dp[i] = Math.max(nums[i] + dp[ i -2] , dp[i - 1]);
-        }
-        return dp[n - 1];
+            if(n == 0){
+                return dp[n] = nums[0];
+            }
+            return robmem(nums , n - 1, dp);
+
+         // using tabulation method //
+        //  return robtab(nums);
     }
-   
+    // int robtab(int nums[]){
+    //     int n = nums.length;
+    //     if(n == 1){
+    //         return nums[0];
+    //     }
+    //     int dp[] = new int[n];
+    //     Arrays.fill(dp , -1);
+    //     dp[0] = nums[0];
+    //     dp[1] = Math.max(nums[0] ,nums[1]);
+    //     for(int i = 2; i < n; i++){
+    //         dp[i] = Math.max(nums[i] + dp[ i -2] , dp[i - 1]);
+    //     }
+    //     return dp[n - 1];
+    // }
+
+    // using memoization method //
+
+    int robmem(int nums[] , int n , int dp[]){
+        if( n == 0){
+            return dp[n] = nums[0];
+        }
+        if(n < 0){
+            return 0;
+        }
+        if(dp[n] != -1){
+            return dp[n];
+        }
+        dp[n] = Math.max(nums[n] + robmem(nums , n - 2 , dp) , robmem(nums , n - 1 , dp));
+    return dp[n];   
+    }
 }
