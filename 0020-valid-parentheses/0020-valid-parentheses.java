c07e398fb466bb0c8c364f3cfe99dd0612ecd1dc
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-04 14:14:03 +0530

    Time: 3 ms (87.45%), Space: 43 MB (94.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> st = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } 
+            else {
+                if (st.isEmpty()){
+                     return false; 
+                }
+                char top = st.peek();
+                if ((c == ')' && top != '(') || 
+                    (c == '}' && top != '{') || 
+                    (c == ']' && top != '[')) {
+                    return false; 
+                }
+                st.pop();
+            }
+        }
+        return st.isEmpty();
+    }
+}
+
