f505b0621c3278dcbb133d3d40cbf82553c19daa
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-08 22:24:22 +0530

    Time: 7 ms (70.20%) | Memory: 46.6 MB (20.76%) - LeetSync

--- 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
+++ 583-delete-operation-for-two-strings/delete-operation-for-two-strings.java
@@ -9,13 +9,14 @@ class Solution {
         for(int j = 0; j <= m; j++){
             dp[0][j] = j;
         }
+
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
-            if(word1.charAt(i - 1) == word2.charAt(j - 1)){
-               dp[i][j] = dp[i - 1][j - 1];
-            }else{
-                 dp[i][j] =1 +  Math.min(dp[i - 1][j] , dp[i][j - 1]);
-            }
+                if(word1.charAt(i - 1) == word2.charAt(j - 1)){
+                    dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i - 1][j] , dp[i][j - 1]);
+                }
             }
         }
         return dp[n][m];
