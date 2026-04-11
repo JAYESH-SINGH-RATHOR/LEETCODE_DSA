a9567e17d84e2c08d08c81421f94679fc2510c3d
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-11 17:58:59 +0530

    Time: 16 ms (65.09%), Space: 46.9 MB (31.18%) - LeetHub

--- 0322-coin-change/0322-coin-change.java
+++ 0322-coin-change/0322-coin-change.java
@@ -1,23 +1,25 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-         int n = coins.length;
-        int dp[][] = new int[n + 1][amount + 1];
-        for(int i = 0; i <= n; i++){
-            dp[i][0] = 0;
-        }
-        for(int i = 0; i <= amount; i++){
-            dp[0][i] = Integer.MAX_VALUE - 1;
-        }
-        for(int i = 1; i <= n; i++){
-            for(int j = 1; j <= amount; j++){
-                int include = Integer.MAX_VALUE - 1;
-                if(coins[i - 1] <= j){
-                    include =  1 + dp[i][j - coins[ i - 1]];
-                }
-                int exclude = dp[i - 1][j];
-                dp[i][j] = Math.min(include , exclude);
+            int n = coins.length;
+            int dp[][] = new int[n + 1][amount + 1];
+            for(int i = 0; i <= n; i++){
+                dp[i][0] = 0;
             }
-        }
+            for(int j = 0; j <= amount; j++){
+                dp[0][j] = Integer.MAX_VALUE - 1;
+            }
+            for(int i = 1; i <= n; i++){
+                for(int j = 1; j <= amount; j++){
+                    int include = Integer.MAX_VALUE - 1;
+                    if(coins[i - 1] <= j){
+                    include = 1 + dp[i][j - coins[i - 1]];
+                    }  
+                    int exclude = dp[i - 1][j];
+                    dp[i][j] = Math.min(include , exclude);
+                }
+            }
         return dp[n][amount] == Integer.MAX_VALUE - 1 ? -1 : dp[n][amount];
     }
 }
+
+
