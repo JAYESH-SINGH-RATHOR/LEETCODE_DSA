a06c003d94a6134a22dfbde392f0f09023e330bd
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-04-12 15:21:16 +0530

    Time: 5 ms (69.63%) | Memory: 47.3 MB (40.22%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -4,25 +4,19 @@ class Solution {
         int m = word2.length();
         int dp[][] = new int[n + 1][m + 1];
         for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= m; j++){
-                if(i == 0){
-                    dp[i][j] = j;
-                }else if(j == 0){
-                    dp[i][j] = i;
-                }
-            }
+            dp[i][0] = i;
         }
+        for(int j = 0; j <= m; j++){
+            dp[0][j] = j;
+        }
+
         for(int i = 1; i <= n; i++){
             for(int j = 1; j <= m; j++){
                 if(word1.charAt(i - 1) == word2.charAt(j - 1)){
                     dp[i][j] = dp[i - 1][j - 1];
+                }else{
+                    dp[i][j] = 1 + Math.min(dp[i][j - 1] , Math.min(dp[i - 1][j - 1] , dp[i - 1][j]));
                 }
-            else{
-                int add = dp[i][j - 1] + 1;
-                int rep = dp[i - 1][j - 1] + 1;
-                int rem = dp[i - 1][j] + 1;
-                dp[i][j] = Math.min(add , Math.min(rep , rem));
-            }
             }
         }
         return dp[n][m];
