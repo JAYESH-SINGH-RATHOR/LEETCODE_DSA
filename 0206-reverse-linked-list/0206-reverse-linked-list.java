0937176d422b31d2679cd7e99204b2fa793467f2
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-12 00:23:32 +0530

    Time: 0 ms (100%), Space: 44.3 MB (29.6%) - LeetHub

---
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -0,0 +1,15 @@
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode temp = head;
+        ListNode prev = null;
+        ListNode next;
+        while(temp != null){
+            next = temp.next;
+            temp.next = prev;
+            prev = temp;
+            temp = next;
+        }
+         head = prev;
+         return head;
+    }
+}
