eef6cb3187a6341114dfbbe34e1814e13555b13d
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-20 17:51:26 +0530

    Time: 1 ms (99.93%) | Memory: 69.1 MB (14.41%) - LeetSync

--- 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
+++ 209-minimum-size-subarray-sum/minimum-size-subarray-sum.java
@@ -1,17 +1,32 @@
 public class Solution {
+    // public int minSubArrayLen(int target, int[] nums) {
+    //    int left = 0;
+    //    int windowsize = Integer.MAX_VALUE;
+    //    int sum = 0;
+    //    for(int i = 0; i < nums.length; i++){
+    //         sum += nums[i];
+    //         while(sum >= target){
+    //             windowsize = Math.min(windowsize , i - left + 1);
+    //             sum -= nums[left];
+    //             left++;
+    //         } 
+    //    }
+    //         return windowsize == Integer.MAX_VALUE ? 0 : windowsize;
+    // }
+
     public int minSubArrayLen(int target, int[] nums) {
-       int left = 0;
-       int windowsize = Integer.MAX_VALUE;
-       int sum = 0;
-       for(int i = 0; i < nums.length; i++){
+        int n = nums.length;
+        int size = Integer.MAX_VALUE;
+        int sum = 0;
+        int left = 0;
+        for(int i = 0; i < n; i++){
             sum += nums[i];
             while(sum >= target){
-                windowsize = Math.min(windowsize , i - left + 1);
+                size = Math.min(size , i - left + 1);
                 sum -= nums[left];
                 left++;
-            } 
-       }
-            return windowsize == Integer.MAX_VALUE ? 0 : windowsize;
+            }
+        }
+        return size == Integer.MAX_VALUE ? 0 : size;
     }
-    
 }
