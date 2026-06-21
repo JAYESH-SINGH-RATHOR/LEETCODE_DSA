5dca8d06e590e7a6801381bf4c954af22f020044
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-21 21:58:24 +0530

    Time: 6 ms (96.33%), Space: 45.1 MB (44.26%) - LeetHub

---
+++ 0179-largest-number/0179-largest-number.java
@@ -0,0 +1,25 @@
+public class Solution {
+    public String largestNumber(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return "";
+        }
+        String[] strs = new String[nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            strs[i] = String.valueOf(nums[i]);
+        }
+        Arrays.sort(strs, new Comparator<String>() {
+            // @Override
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);
+            }
+        });
+        if (strs[0].equals("0")) {
+            return "0";
+        }
+        StringBuilder sb = new StringBuilder();
+        for (String s : strs) {
+            sb.append(s);
+        }
+        return sb.toString();
+    }
+}
