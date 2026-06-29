465b1a0966618e8a82c07bb6bb8a17f56e64e5c0
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-29 23:17:29 +0530

    Time: 1 ms (78.06%), Space: 46.8 MB (42.43%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,75 +1,11 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0){
-            return 0;
-        }
-        int  n = nums.length;
-        for(int i = 0; i < n - 1; i++){
-            if(nums[i] == nums[i + 1]){
-                for(int j = i + 1; j < n - 1; j++){
-                    nums[j] = nums[j  + 1];
-                }
-                n--;
-                i--;
+        int k = 1;
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i] != nums[ i - 1 ]){
+                nums[k++] = nums[i];
             }
-        }    
-        return n;
+        }
+            return k;
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//    if (nums.length == 0)
-//         {
-//              return 0;
-//         }
-//         int i = 0; 
-//         for (int j = 0; j < nums.length; j++) {
-//             if (nums[j] != nums[i]) {
-//                 i++;
-//                 nums[i] = nums[j];
-//             }
-//         }
-//         return i + 1;
