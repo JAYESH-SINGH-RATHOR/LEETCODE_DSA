114df6e1179d3751c3b8eabe0568b743bb0941ef
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-06-29 19:45:04 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (19.94%) - LeetSync

---
+++ 2099-number-of-strings-that-appear-as-substrings-in-word/number-of-strings-that-appear-as-substrings-in-word.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int numOfStrings(String[] patterns, String word) {
+       int count = 0;
+       for(String e : patterns){
+        if(word.contains(e)){
+            count++;
+        }
+       }
+       return count;
+    }
+}
