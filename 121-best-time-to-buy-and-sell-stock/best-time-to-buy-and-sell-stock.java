34e2f26fd9a0e1f44e992ed5627f04127a3286a2
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-01 16:43:24 +0530

    Time: 1 ms (99.94%) | Memory: 94.4 MB (70.00%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,28 +1,11 @@
 class Solution {
-    // public int maxProfit(int[] prices) {
-    //   int n = prices.length;
-    //   int buyprice = Integer.MAX_VALUE;
-    //   int maxprofit = 0;
-    //   for(int i = 0; i < n; i++){
-    //     if(buyprice < prices[i]){
-    //         int profit = prices[i] - buyprice;
-    //         maxprofit = Math.max(maxprofit , profit);
-    //     }else{
-    //         buyprice = prices[i];
-    //     }
-    //   }
-    //   return maxprofit;
-    // }
-
     public int maxProfit(int[] prices) {
         int buyp = Integer.MAX_VALUE;
         int profit = 0;
-        int n = prices.length;
-        for(int i = 0; i < n; i++){
+        for(int i = 0; i < prices.length; i++){
             buyp = Math.min(buyp , prices[i]);
             profit = Math.max(profit , prices[i] - buyp);
         }
         return profit;
     }
 }
- 
