7d63f3fbfae69c255e63aa30d5d4b08be3a755ab
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-13 00:52:35 +0530

    Time: 42 ms (23.88%), Space: 76.1 MB (49.13%) - LeetHub

--- 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -1,24 +1,46 @@
-class Solution {
-    public int[] arrayRankTransform(int[] arr) {
-        ArrayList<Integer> index = new ArrayList<>();
-        for(int i = 0; i < arr.length; i++){
-            index.add(i);
-        }
-        Collections.sort(index , new Comparator<Integer>(){
-            public int compare(Integer x , Integer y){
-                return arr[x] - arr[y];
-            }
-        });
-        int rank = 0;
-        int prev = Integer.MAX_VALUE;
-        int res[] = new int[arr.length];
-        for(int e : index){
-            if(prev != arr[e]){
-                prev = arr[e];
-                rank++;
-            }
-            res[e] = rank;
-        }
-        return res;
-    }
+// class Solution {
+//     public int[] arrayRankTransform(int[] arr) {
+//         ArrayList<Integer> index = new ArrayList<>();
+//         for(int i = 0; i < arr.length; i++){
+//             index.add(i);
+//         }
+//         Collections.sort(index , new Comparator<Integer>(){
+//             public int compare(Integer x , Integer y){
+//                 return arr[x] - arr[y];
+//             }
+//         });
+//         int rank = 0;
+//         int prev = Integer.MAX_VALUE;
+//         int res[] = new int[arr.length];
+//         for(int e : index){
+//             if(prev != arr[e]){
+//                 prev = arr[e];
+//                 rank++;
+//             }
+//             res[e] = rank;
+//         }
+//         return res;
+//     }
+// }
+
+
+// secound appraoch 
+
+public class Solution {
+     public int[] arrayRankTransform(int[] arr){
+         TreeSet<Integer> set = new TreeSet<>();
+       Map<Integer, Integer> map = new HashMap<>();
+       for(int e : arr){
+            set.add(e);
+       }
+       int rank = 1;
+       for(int e : set){
+            map.put(e , rank);
+            rank++;
+       }
+       for(int i = 0; i < arr.length; i++){
+        arr[i] = map.get(arr[i]);
+       }
+    return arr;
+     }
 }
