0d1d805abe5fd75162e436badbd912fd634b5087
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-29 10:53:31 +0530

    Time: 7 ms (31.95%), Space: 44.4 MB (66.84%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,27 +1,49 @@
+// class Solution {
+//     public boolean isAnagram(String s, String t) {
+//         if(s.length() != t.length()){
+//             return false;
+//         }
+//         HashMap<Character , Integer > map = new HashMap<>();
+//         for(int i = 0; i < s.length(); i++){
+//             char ch = s.charAt(i);
+//             if(!map.containsKey(ch)){
+//                 map.put(ch , 0);
+//             }
+//             int freq = map.get(ch);
+//             map.put(ch , freq + 1);
+//         }
+//         for(int i = 0; i < t.length(); i++){
+//             char ch = t.charAt(i);
+//             if(!map.containsKey(ch)){
+//                 return false;
+//             }
+//             map.put(ch , map.get(ch) - 1);
+//             if(map.get(ch) == 0){
+//                 map.remove(ch);
+//             }
+//         }
+//         return map.isEmpty();
+//     }
+// }
+
+
+// secound appraoch // 
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
-        HashMap<Character , Integer > map = new HashMap<>();
+        int freq[] = new int[26];
         for(int i = 0; i < s.length(); i++){
-            char ch = s.charAt(i);
-            if(!map.containsKey(ch)){
-                map.put(ch , 0);
-            }
-            int freq = map.get(ch);
-            map.put(ch , freq + 1);
+            freq[s.charAt(i) - 'a']++;
+            freq[t.charAt(i) - 'a']--;
         }
-        for(int i = 0; i < t.length(); i++){
-            char ch = t.charAt(i);
-            if(!map.containsKey(ch)){
+        for(int c : freq){
+            if(c != 0){
                 return false;
             }
-            map.put(ch , map.get(ch) - 1);
-            if(map.get(ch) == 0){
-                map.remove(ch);
-            }
         }
-        return map.isEmpty();
+        return true;
     }
 }
