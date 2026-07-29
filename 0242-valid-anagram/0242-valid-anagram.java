8851cca4deec3ff0cf377fa3fdf68f7529b166e2
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-29 10:48:11 +0530

    Time: 20 ms (8.76%), Space: 46.6 MB (25.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,61 +1,27 @@
 class Solution {
-
-            // method 1
-    // public boolean isAnagram(String s, String t) {
-    //     if (s.length() != t.length()) {
-    //         return false;
-
-    //     }
-    //     char[] sArr = s.toCharArray();
-    //     char[] tArr = t.toCharArray();
-    //     Arrays.sort(sArr);
-    //     Arrays.sort(tArr);
-
-    //     return Arrays.equals(sArr, tArr);
-    // }
-
-    // method 2
-
-    // public boolean isAnagram(String s, String t) {
-    //     if (s.length() != t.length()) {
-    //         return false;
-    //     }
-    //     HashMap<Character, Integer> map = new HashMap<>(); 
-    //     for(int i = 0; i < s.length(); i++){
-    //         char ch = s.charAt(i);
-    //         if(!map.containsKey(ch)){
-    //             map.put(ch , 0);
-    //         }
-    //         int freq = map.get(ch);
-    //         map.put(ch , freq + 1);
-    //     }    
-    //     for(int i = 0; i < t.length(); i++){
-    //         char ch = t.charAt(i);
-          
-    //         if(!map.containsKey(ch) || map.get(ch) == 0){
-    //             return false;
-    //         }
-    //         map.put(ch , map.get(ch) - 1);
-    //     }
-    //     return true;
-    // }
-
-    // method 3
-     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) {
+    public boolean isAnagram(String s, String t) {
+        if(s.length() != t.length()){
             return false;
         }
-        int freq[] = new int[26];
+        HashMap<Character , Integer > map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+            char ch = s.charAt(i);
+            if(!map.containsKey(ch)){
+                map.put(ch , 0);
+            }
+            int freq = map.get(ch);
+            map.put(ch , freq + 1);
         }
-
-        for(int x : freq){
-            if(x != 0){
+        for(int i = 0; i < t.length(); i++){
+            char ch = t.charAt(i);
+            if(!map.containsKey(ch)){
                 return false;
+            }
+            map.put(ch , map.get(ch) - 1);
+            if(map.get(ch) == 0){
+                map.remove(ch);
             }
         }
         return true;
-     }
+    }
 }
