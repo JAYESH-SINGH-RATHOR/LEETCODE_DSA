6197caac325838c9a42dee5d58660eff4f99d4cc
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-07-31 17:40:27 +0530

    Time: 0 ms (100%), Space: 48.2 MB (32.3%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,19 +1,66 @@
+// appraoch 1 O(N)
+
+// class Solution {
+//     public int[] searchRange(int[] nums, int target) {
+//         int first = -1;
+//         int sec = -1;
+//         for(int i = 0; i < nums.length; i++){
+//             if(nums[i] == target){
+//                 first = i;
+//                 break;
+//             }
+//         }
+//         for(int i = nums.length - 1; i >= 0; i--){
+//             if(nums[i] == target){
+//                 sec = i;
+//                 break;
+//             }
+//         }
+//         return new int[]{first , sec};
+//     }
+// }
+
+
+// appraoch O(lon n)
+
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int first = -1;
-        int sec = -1;
-        for(int i = 0; i < nums.length; i++){
-            if(nums[i] == target){
-                first = i;
-                break;
+      int first = findFirst(nums , target);
+      int  last = findLast(nums , target);
+      return new int[] {first , last};
+    }
+    int findFirst(int arr[] , int t){
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == t){
+                ans = mid;
+                right = mid - 1;
+            }else if(arr[mid] < t){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
             }
         }
-        for(int i = nums.length - 1; i >= 0; i--){
-            if(nums[i] == target){
-                sec = i;
-                break;
+        return ans;
+    }
+    int findLast(int arr[] , int t){
+        int left = 0;
+        int right = arr.length - 1;
+        int ans = -1;
+        while(left <= right){
+            int mid = left + (right - left)/2;
+            if(arr[mid] == t){
+                ans = mid;
+                left = mid + 1;
+            }else if(arr[mid] < t){
+                left = mid + 1;
+            }else{
+                right = mid - 1;
             }
         }
-        return new int[]{first , sec};
+        return ans;
     }
 }
