b361a0a4af70414ba8a58d37a3db54db53eb8183
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-03 01:17:32 +0530

    Time: 22 ms (7.01%), Space: 50.8 MB (5.1%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.java
@@ -1,26 +1,25 @@
-public class Solution {
+class Solution {
     public int[] topKFrequent(int[] nums, int k) {
-        int n = nums.length;
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int i = 0; i < n; i++){
-            if(!map.containsKey(nums[i])){
-                map.put(nums[i] , 0);
-            }
-            int freq = map.get(nums[i]);
-            map.put(nums[i] , freq + 1);
+       HashMap<Integer , Integer> map = new HashMap<>();
+PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> map.get(a) - map.get(b));
+       for(int e : nums){
+        if(!map.containsKey(e)){
+            map.put(e , 0);
         }
-PriorityQueue<Integer> pq = new
- PriorityQueue<>((a, b) -> Integer.compare(map.get(a), map.get(b)));
-        for(int e : map.keySet()){
-            pq.add(e);
-            while(pq.size() > k){
-                pq.remove();
-            }
+        int freq = map.get(e);
+        map.put(e , freq + 1);
+       }
+       for(int e : map.keySet()){
+        pq.add(e);
+        while(pq.size() > k){
+            pq.remove();
         }
-        int ans[] = new int[k];
-        for(int i = k - 1; i >= 0; i--){
-            ans[i] = pq.remove();
-        }
-        return ans;
+       }
+       System.out.print(map);
+       int res[] = new int[k];
+       for(int i = k - 1; i >= 0; i--){
+        res[i] = pq.remove();
+       }
+       return res;
     }
 }
