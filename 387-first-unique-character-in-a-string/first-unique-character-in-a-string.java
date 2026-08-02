e6657fd0c9d81f25ff662d4b8da180ba350a8b2e
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-03 00:55:46 +0530

    Time: 39 ms (6.21%) | Memory: 46.9 MB (58.72%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,16 +1,42 @@
-class Solution {
-    public int firstUniqChar(String s) {
-        int arr[] = new int[26];
+
+        // optimal appraoch 1
+// class Solution {
+//     public int firstUniqChar(String s) {
+//         int arr[] = new int[26];
+//         for(int i = 0; i < s.length(); i++){
+//             char ch = s.charAt(i);
+//             arr[ch - 'a']++;
+//         }
+//         for(int i = 0; i <s.length(); i++){
+//            char ch = s.charAt(i);
+//            if(arr[ch - 'a'] == 1){
+//             return i;
+//             }
+//         }
+//         return -1;
+//     }
+// }
+
+// optimal appraoch 2 ///
+
+public class Solution{
+     public int firstUniqChar(String s) {
+        HashMap<Character , Integer> map = new HashMap<>();
         for(int i = 0; i < s.length(); i++){
             char ch = s.charAt(i);
-            arr[ch - 'a']++;
+            if(!map.containsKey(ch)){
+                map.put(ch , 0);
+            }
+            int freq = map.get(ch);
+            map.put(ch , freq + 1);
         }
-        for(int i = 0; i <s.length(); i++){
-           char ch = s.charAt(i);
-           if(arr[ch - 'a'] == 1){
-            return i;
+        for(int i = 0; i < s.length(); i++){
+            char ch = s.charAt(i);
+            if(map.get(ch) == 1){
+                return i;
             }
         }
         return -1;
-    }
+     }
 }
+
