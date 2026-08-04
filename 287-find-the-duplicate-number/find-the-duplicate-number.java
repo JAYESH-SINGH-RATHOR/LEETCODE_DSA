2d29fce23f0c99dfaf93d6caa26cb4a4c217500e
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-04 08:27:51 +0530

    Time: 43 ms (6.12%) | Memory: 92.9 MB (5.91%) - LeetSync

--- 287-find-the-duplicate-number/find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/find-the-duplicate-number.java
@@ -13,19 +13,36 @@
 //     }
 // }
 
-// optimized appraoch 
+// optimized appraoch O(n log n )
+
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//         Arrays.sort(nums);
+//         int left = 0;
+//         int right = 1;
+//         while(right < nums.length){
+//             if(nums[left] == nums[right]){
+//                 return nums[left];
+//             }
+//             left++;
+//             right++;
+//         }
+//         return -1;
+//     }
+// }
 
 class Solution {
     public int findDuplicate(int[] nums) {
-        Arrays.sort(nums);
-        int left = 0;
-        int right = 1;
-        while(right < nums.length){
-            if(nums[left] == nums[right]){
-                return nums[left];
+        HashMap<Integer , Integer > map = new HashMap<>();
+        for(int e : nums){
+            if(!map.containsKey(e)){
+                map.put(e , 0);
             }
-            left++;
-            right++;
+            int freq = map.get(e);
+            map.put(e , freq + 1);
+            if(map.get(e) > 1){
+                return e;
+            }
         }
         return -1;
     }
