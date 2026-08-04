ce1cc9e1bacbb49e0c2b877ea97575a027ffa37d
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-04 08:38:47 +0530

    Time: 6 ms (43.01%), Space: 44.2 MB (82.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,46 +1,16 @@
-// class Solution {
-//     public boolean isAnagram(String s, String t) {
-//         if(s.length() != t.length()){
-//             return false;
-//         }
-//         HashMap<Character , Integer > map = new HashMap<>();
-//         for(int i = 0; i < s.length(); i++){
-//             char ch = s.charAt(i);
-//             if(!map.containsKey(ch)){
-//                 map.put(ch , 0);
-//             }
-//             int freq = map.get(ch);
-//             map.put(ch , freq + 1);
-//         }
-//         for(int i = 0; i < t.length(); i++){
-//             char ch = t.charAt(i);
-//             if(!map.containsKey(ch)){
-//                 return false;
-//             }
-//             map.put(ch , map.get(ch) - 1);
-//             if(map.get(ch) == 0){
-//                 map.remove(ch);
-//             }
-//         }
-//         return map.isEmpty();
-//     }
-// }
-
-
-// secound appraoch // 
-
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()){
             return false;
         }
-        int freq[] = new int[26];
+        int res[] = new int[26];
         for(int i = 0; i < s.length(); i++){
-            freq[s.charAt(i) - 'a']++;
-            freq[t.charAt(i) - 'a']--;
+            char ch = s.charAt(i);
+            res[ch - 'a']++;
+            res[t.charAt(i) - 'a']--;
         }
-        for(int c : freq){
-            if(c != 0){
+        for(int e : res){
+            if(e != 0){
                 return false;
             }
         }
