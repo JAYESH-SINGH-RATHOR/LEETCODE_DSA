0713bdb514cb0aa6696878fa2f2d262f3664896e
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-04 08:24:29 +0530

    Time: 37 ms (16.18%), Space: 79.3 MB (89.9%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -1,32 +1,32 @@
+
+        // brute force appraoch
+// class Solution {
+//     public int findDuplicate(int[] nums) {
+//        for(int i = 0; i < nums.length; i++){
+//         for(int j = i + 1; j < nums.length; j++){
+//             if(nums[i] == nums[j]){
+//                 return nums[i];
+//             }
+//         }
+//        }
+//        return -1;
+//     }
+// }
+
+// optimized appraoch 
+
 class Solution {
     public int findDuplicate(int[] nums) {
-        HashMap<Integer , Integer> map = new HashMap<>();
-        for(int e : nums){
-            if(!map.containsKey(e)){
-                map.put(e , 0);
+        Arrays.sort(nums);
+        int left = 0;
+        int right = 1;
+        while(right < nums.length){
+            if(nums[left] == nums[right]){
+                return nums[left];
             }
-            int freq = map.get(e);
-            map.put(e , freq + 1);
-        }
-        for(int key : map.keySet()){
-            int freq = map.get(key);
-            if(freq > 1){
-               return key;
-            }
+            left++;
+            right++;
         }
         return -1;
-    //     int slow = nums[0];
-    //     int fast = nums[0];
-    //     do{
-    //         slow = nums[slow];
-    //         fast = nums[nums[fast]];
-    //     }
-    //     while(slow != fast);
-    //     slow = nums[0];
-    //     while(slow != fast){            
-    //     slow = nums[slow];
-    //     fast = nums[fast];
-    //     }
-    // return slow;
     }
 }
