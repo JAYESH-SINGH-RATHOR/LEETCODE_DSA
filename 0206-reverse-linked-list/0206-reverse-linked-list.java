95e8fcb6e61d3ff6ab994ec6e1b4648c277de4c4
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-10 19:57:59 +0530

    Time: 0 ms (100%), Space: 44.2 MB (50.55%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -1,17 +1,35 @@
+// class Solution {
+//     public ListNode reverseList(ListNode head) {
+//        Stack<Integer> st = new Stack<>();
+//        ListNode temp = head;
+//        while(temp != null){
+//         st.push(temp.val);
+//         temp = temp.next;
+//        }
+//         System.out.print(st);
+//         temp = head;
+//         while(!st.isEmpty()){
+//             temp.val = st.pop();
+//             temp = temp.next;
+//         }
+//         return head;
+//     }
+// }
+
+
+// secound approach 2// 
+
 class Solution {
     public ListNode reverseList(ListNode head) {
-       Stack<Integer> st = new Stack<>();
-       ListNode temp = head;
-       while(temp != null){
-        st.push(temp.val);
-        temp = temp.next;
-       }
-        System.out.print(st);
-        temp = head;
-        while(!st.isEmpty()){
-            temp.val = st.pop();
-            temp = temp.next;
-        }
-        return head;
+        ListNode curr = head;
+        ListNode prev = null;
+        ListNode next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        } 
+        return prev;
     }
 }
