241597688dab3edcbf2ddb267ca386bd2e21fb66
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-13 20:10:12 +0530

    Time: 0 ms (100%), Space: 42.2 MB (17.75%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,28 +1,27 @@
-class Solution {
+ // bruteforce way 
 
-    // using bottom up technique
-    public int climbStairs(int n) {
-        int dp[] = new int[n + 1];
-        dp[0] = 1;
-        dp[1] = 1;
-        for(int i = 2; i <= n; i++){
-            dp[i] = dp[i - 1] + dp[i - 2];
+// class Solution {
+//     public int climbStairs(int n) {
+//         if(n <= 2){
+//             return n;
+//         }
+//         return climbStairs(n - 1) + climbStairs(n - 2);
+//     }
+// }
+
+// optimized way 
+
+class Solution{
+    public int climbStairs(int n){
+        if(n <= 2){
+            return n;
         }
-        return dp[n];
+        int dp[] = new int[n];
+        dp[0] = 1;
+        dp[1] = 2;
+        for(int i = 2; i < n; i++){
+            dp[i] = dp[i - 1 ] + dp[i - 2]; 
+        }
+        return dp[n - 1];
     }
-
-    // using memoization technique
-    //     public int climbStairs(int n){
-    //         int dp[] = new int[n + 1];
-    //         return climbStairs1(n , dp);
-    //     }
-    // public int climbStairs1(int n , int dp[]) {
-    //     if(n == 0){
-    //         return 1;
-    //     }
-    //     if(n <= 2){
-    //         return n;
-    //     }
-    //     return climbStairs1(n - 1 , dp) + climbStairs1(n - 2 , dp);
-    // }
 }
