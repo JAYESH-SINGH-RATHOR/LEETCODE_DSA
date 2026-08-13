3690d3fa99c5545b97d0ba3e2cadcfb60a5f4cf9
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-13 20:30:19 +0530

    Time: 0 ms (100%), Space: 41.8 MB (87.39%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -10,18 +10,40 @@
 // }
 
 // optimized way 
+    // method 1 > -> tabultaion Method(Bottom Up Method)
+// class Solution{
+//     public int climbStairs(int n){
+//         if(n <= 2){
+//             return n;
+//         }
+//         int dp[] = new int[n];
+//         dp[0] = 1;
+//         dp[1] = 2;
+//         for(int i = 2; i < n; i++){
+//             dp[i] = dp[i - 1 ] + dp[i - 2]; 
+//         }
+//         return dp[n - 1];
+//     }
+// }
+
+// Method 2 -> memoization method (recursion Method) //
 
 class Solution{
     public int climbStairs(int n){
         if(n <= 2){
             return n;
         }
-        int dp[] = new int[n];
-        dp[0] = 1;
-        dp[1] = 2;
-        for(int i = 2; i < n; i++){
-            dp[i] = dp[i - 1 ] + dp[i - 2]; 
+        int dp[] = new int[n + 1];
+        return recdp(n , dp);
+    }
+    int recdp(int n , int dp[]){
+        if(n <= 2){
+            return n;
         }
-        return dp[n - 1];
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        dp[n] = recdp(n - 1 , dp) + recdp(n - 2 , dp);
+        return dp[n];
     }
 }
