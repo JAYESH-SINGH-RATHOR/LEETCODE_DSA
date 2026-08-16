058404629ba52a569f66676ca5111810b091420e
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-16 17:28:52 +0530

    Time: 6 ms (46.00%) | Memory: 41.7 MB (93.81%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.java
+++ 1013-fibonacci-number/fibonacci-number.java
@@ -10,18 +10,42 @@
 //     }
 // }
 
+// using tabulation dp 
+
+// class Solution {
+//     public int fib(int n) {
+//       if(n == 0){
+//         return 0;
+//       }
+//       int dp[] = new int[n + 1];
+//       dp[0] = 0;
+//       dp[1] = 1;
+//       for(int i = 2; i <= n; i++){
+//         dp[i] = dp[i - 1] + dp[i - 2];
+//       }
+//       return dp[n];
+//     }
+// }
+
+
+// using memoization dp
 
 class Solution {
     public int fib(int n) {
-      if(n == 0){
-        return 0;
-      }
-      int dp[] = new int[n + 1];
-      dp[0] = 0;
-      dp[1] = 1;
-      for(int i = 2; i <= n; i++){
-        dp[i] = dp[i - 1] + dp[i - 2];
-      }
-      return dp[n];
+        if(n == 0){
+            return 0;
+        }
+        if(n <= 2){
+            return 1;
+        }
+        int dp[] = new int[n + 1];
+        return recdp(n , dp);
+    }
+    int recdp(int n , int dp[]){
+        if(n <= 2){
+            return 1;
+        }
+        dp[n] = recdp(n - 1 , dp) + recdp(n - 2 , dp);
+        return dp[n];
     }
 }
