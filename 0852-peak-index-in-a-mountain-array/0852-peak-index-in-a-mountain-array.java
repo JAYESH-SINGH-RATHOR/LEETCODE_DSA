a68012abb43d1da2ff40f7e4da9473d96e3aff18
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-22 05:15:20 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.java
@@ -2,18 +2,14 @@ class Solution {
     public int peakIndexInMountainArray(int[] arr) {
        int left = 0;
        int right = arr.length - 1;
-       int max = Integer.MIN_VALUE;
-       while(left <= right){
-        int mid  = left + (right - left) / 2;
-        if(arr[mid] == max){
-            return mid;
-        }else if(arr[mid] > max){
-            max = mid;
-            right = mid - 1;
-        }else{
-            left = mid + 1;
+        while(left < right){
+            int mid = left + (right - left) /2;
+            if(arr[mid] < arr[mid + 1]){
+                left = mid + 1;
+            }else{
+                right = mid;
+            }
         }
-       }
-       return max;
+        return left;
     }
 }
