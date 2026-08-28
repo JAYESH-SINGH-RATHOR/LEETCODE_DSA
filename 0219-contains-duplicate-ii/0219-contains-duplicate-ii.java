0e958cd92694266f5d8eb95a48987a9e526edfd5
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-08-28 20:39:37 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.java
@@ -1,39 +1,12 @@
-public class Solution {
+class Solution {
     public boolean containsNearbyDuplicate(int[] nums, int k) {
-        Set<Integer> set = new HashSet<>();
-
-        for (int i = 0; i < nums.length; i++) {
-            if (set.contains(nums[i])) {
+       for(int i = 0; i < nums.length; i++){
+        for(int j =  i + 1;  j < nums.length && j <= i + k; j++){
+            if(nums[i] == nums[j]){
                 return true;
             }
-
-            set.add(nums[i]);
-
-            if (set.size() > k) {
-                set.remove(nums[i - k]);
-            }
         }
-
-        return false;
+       }
+       return false;
     }
-
-    // public boolean containsNearbyDuplicate(int[] nums, int k) {
-    //     int n = nums.length;
-    //     Set<Integer>set = new HashSet<>();
-    //     for(int i = 0; i < n && i < k; i++){
-    //         if(set.contains(nums[i])){
-    //             return true;
-    //         }
-    //         set.add(nums[i]);
-    //     }
-    //     for(int i = k; i < n; i++){
-    //         int window = nums[i - k];
-    //         if(set.contains(nums[i])){
-    //             return true;
-    //         }
-    //         set.add(nums[i]);
-    //         set.remove(window);
-    //     }
-    //     return false;
-    // }
 }
