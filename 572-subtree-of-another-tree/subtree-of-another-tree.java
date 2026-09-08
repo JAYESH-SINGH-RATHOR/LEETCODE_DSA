4bc7769ef160145d5052bb6a57f9075873f1cdd5
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-08 22:13:51 +0530

    Time: 2 ms (99.14%) | Memory: 46.3 MB (70.55%) - LeetSync

--- 572-subtree-of-another-tree/subtree-of-another-tree.java
+++ 572-subtree-of-another-tree/subtree-of-another-tree.java
@@ -15,29 +15,26 @@
  */
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
-        if(root == null){
+        if (root == null) {
             return false;
         }
-        if(root.val == subRoot.val){
-            if(identical(root , subRoot)){
-                return true;
-            }
-        }
-        return isSubtree(root.left , subRoot) || isSubtree(root.right , subRoot);
-    }
-    public boolean identical(TreeNode root , TreeNode subRoot){
-        if(root == null && subRoot == null){
+        if (isSameTree(root, subRoot)) {
             return true;
         }
-        if(root == null || subRoot == null || root.val != subRoot.val){
+        return isSubtree(root.left, subRoot) ||
+               isSubtree(root.right, subRoot);
+    }
+    public boolean isSameTree(TreeNode root, TreeNode subRoot) {
+        if (root == null && subRoot == null) {
+            return true;
+        }
+        if (root == null || subRoot == null) {
             return false;
         }
-        if(!identical(root.right , subRoot.right)){
+        if (root.val != subRoot.val) {
             return false;
         }
-        if(!identical(root.left , subRoot.left)){
-            return false;
-        }
-        return true;
+        return isSameTree(root.left, subRoot.left) &&
+               isSameTree(root.right, subRoot.right);
     }
 }
