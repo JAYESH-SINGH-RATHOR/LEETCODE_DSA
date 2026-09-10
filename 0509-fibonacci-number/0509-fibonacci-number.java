b4061ed0ef698022c09c48ab3bcd89e1bb20b98f
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-10 15:54:40 +0530

    Time: 0 ms (100%), Space: 42 MB (70.16%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,11 +1,34 @@
+// using recursion
+// class Solution {
+//     public int fib(int n) {
+//         if(n == 0){
+//             return 0;
+//         }
+//         if(n < 3){
+//             return 1;
+//         }
+//         return fib(n - 1) + fib(n - 2);
+//     }
+// }
+
+// using memozation dp 
+
 class Solution {
     public int fib(int n) {
+        int dp[] = new int[n + 1];
+        return fabmem(n , dp);
+    }
+    int fabmem(int n , int dp[]){
         if(n == 0){
             return 0;
         }
         if(n < 3){
             return 1;
         }
-        return fib(n - 1) + fib(n - 2);
+        if(dp[n] != 0){
+            return dp[n];
+        }
+        dp[n] = fabmem(n - 1 , dp) + fabmem( n - 2 , dp);
+        return dp[n];
     }
 }
