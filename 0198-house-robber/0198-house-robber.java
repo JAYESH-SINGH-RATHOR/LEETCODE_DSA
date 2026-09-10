3971d8a6db02163d820ad444bc6ccb6f1ddf9cad
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-10 16:36:58 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -6,8 +6,7 @@ class Solution {
         int dp[] = new int[nums.length + 1];
         int n = nums.length;
         dp[0] = nums[0];
-        // dp[1] = Math.max(nums[0] ,  nums[1]);
-        dp[1] = nums[1];
+        dp[1] = Math.max(nums[0] ,  nums[1]);
         for(int i = 2; i < nums.length; i++){
             dp[i] = Math.max(dp[i - 1] , nums[i] + dp[i - 2]);
         }
