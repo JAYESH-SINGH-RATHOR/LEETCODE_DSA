6e4fbc73789e639a5fd786f2925c2a6796fa57ea
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-10 16:04:23 +0530

    Time: 0 ms (100%), Space: 41.7 MB (93.28%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -13,22 +13,39 @@
 
 // using memozation dp 
 
+// class Solution {
+//     public int fib(int n) {
+//         int dp[] = new int[n + 1];
+//         return fabmem(n , dp);
+//     }
+//     int fabmem(int n , int dp[]){
+//         if(n == 0){
+//             return 0;
+//         }
+//         if(n < 3){
+//             return 1;
+//         }
+//         if(dp[n] != 0){
+//             return dp[n];
+//         }
+//         dp[n] = fabmem(n - 1 , dp) + fabmem( n - 2 , dp);
+//         return dp[n];
+//     }
+// }
+
+// using tabulation dp 
+
 class Solution {
     public int fib(int n) {
+       if (n == 0) return 0;
+        if (n == 1) return 1;
+
         int dp[] = new int[n + 1];
-        return fabmem(n , dp);
-    }
-    int fabmem(int n , int dp[]){
-        if(n == 0){
-            return 0;
+        dp[0] =0;
+        dp[1] = 1;
+        for(int i = 2; i <= n; i++){
+            dp[i] = dp[i - 1] + dp[i - 2];
         }
-        if(n < 3){
-            return 1;
-        }
-        if(dp[n] != 0){
-            return dp[n];
-        }
-        dp[n] = fabmem(n - 1 , dp) + fabmem( n - 2 , dp);
         return dp[n];
     }
 }
