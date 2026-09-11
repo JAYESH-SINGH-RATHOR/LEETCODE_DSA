89b4d1e5af46ab926a5b859d1c99f25136264176
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-11 23:08:44 +0530

    Time: 21 ms (84.81%), Space: 178.2 MB (44.16%) - LeetHub

--- 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
+++ 3217-delete-nodes-from-linked-list-present-in-array/3217-delete-nodes-from-linked-list-present-in-array.java
@@ -8,6 +8,8 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
+
+ // optimized way //
 class Solution {
     public ListNode modifiedList(int[] nums, ListNode head) {
       ListNode dummy = new ListNode(0);
@@ -28,3 +30,31 @@ class Solution {
       return dummy.next;
     }
 }
+
+// // bruteforce way // 
+
+// class Solution {
+//     public ListNode modifiedList(int[] nums, ListNode head) {
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+//         ListNode curr = dummy;
+//         while (curr.next != null) {
+//             boolean remove = false;
+//             // Check if curr.next.val is in nums
+//             for (int e : nums) {
+//                 if (curr.next.val == e) {
+//                     remove = true;
+//                     break;
+//                 }
+//             }
+//             if (remove) {
+//                 // Skip the node
+//                 curr.next = curr.next.next;
+//             } else {
+//                 curr = curr.next;
+//             }
+//         }
+
+//         return dummy.next;
+//     }
+// }
