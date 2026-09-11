3ef9d0a520ab6a9d93779211a5f5686ec001507e
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-11 21:43:20 +0530

    Time: 0 ms (100%), Space: 44 MB (92.24%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -8,27 +8,6 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-
- // bruteforce solution 
-// class Solution {
-//     public ListNode reverseList(ListNode head) {
-//         Stack<Integer> st = new Stack<>();
-//         ListNode temp = head;
-//         while(temp != null){
-//             st.push(temp.val);
-//             temp = temp.next;
-//         }
-//         temp = head;
-//         while(temp != null &&!st.isEmpty()){
-//             temp.val = st.pop();
-//             temp = temp.next;
-//         }
-//         return head;
-//     }
-// }
-
-
-// optimzed solutions // 
 class Solution {
     public ListNode reverseList(ListNode head) {
         if(head == null){
@@ -37,11 +16,11 @@ class Solution {
         ListNode curr = head;
         ListNode prev = null;
         ListNode next;
-        while(curr != null ){
-           next = curr.next;
-           curr.next = prev;
-           prev = curr;
-           curr = next;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
         }
         return prev;
     }
