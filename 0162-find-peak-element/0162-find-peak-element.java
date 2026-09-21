5987bce9554e91c31a648fc85a849d9a63f4328e
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-21 19:04:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.java
+++ 0162-find-peak-element/0162-find-peak-element.java
@@ -2,7 +2,7 @@ class Solution {
     public int findPeakElement(int[] nums) {
         Arrays.sort(nums);
         int left = 0;
-        int peak = Integer.MIN_VALUE;
+        int peak = 0;
         int right = nums.length - 1;
         while(left < right){
             int mid = left + (right - left) / 2;
