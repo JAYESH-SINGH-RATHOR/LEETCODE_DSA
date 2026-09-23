6b623f189a9f42e1ce5c497337680bdd0d27e5b5
JAYESH SINGH RATHORE <jayeshsingh971@gmail.com>
2026-09-24 01:42:21 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,17 +1,17 @@
 class Solution {
     public boolean validPath(int n, int[][] edges, int source, int destination) {
-        boolean visited[] = new boolean[n];
-        List<List<Integer>> res = new ArrayList<>();
-        Queue<Integer> q = new LinkedList<>();
+        ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
         for(int i = 0; i < n; i++){
-            res.add(new ArrayList<>());
+            adj.add(new ArrayList<>());
         }
         for(int e[] : edges){
             int u = e[0];
             int v = e[1];
-            res.get(u).add(v);
-            res.get(v).add(u);
-        }        
+            adj.get(u).add(v);
+            adj.get(v).add(u);
+        }
+        boolean visited[] = new boolean[n];
+        Queue<Integer> q = new LinkedList<>();
         q.add(source);
         visited[source] = true;
         while(!q.isEmpty()){
@@ -19,7 +19,7 @@ class Solution {
             if(curr == destination){
                 return true;
             }
-            for(int e : res.get(curr)){
+            for(int e : adj.get(curr)){
                 if(!visited[e]){
                     visited[e] = true;
                     q.add(e);
